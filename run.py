fcf9b86dfa470bae3ca2ae954edcb59e6ff1342f
DavideAllavena <davide.allavena@gmail.com>
2017-10-07 14:10:19 +0200

    Udates in run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import csv
 import tarfile
 import os
 import re
+import sys
 
 def download_file(url, filename):
   r = requests.get(url, stream=True)
@@ -14,7 +15,6 @@ def download_file(url, filename):
 
 def transform_mapping_name(name):
   return re.sub(r".sparql", '.ttl', re.sub(r'^mapping', 'target', name))
-
 
 # Download jarql
 print "Downloading Jarql..."
@@ -41,25 +41,32 @@ with open('mapping.csv', 'r') as mapping_file:
   mapping_dict = csv.DictReader(mapping_file)
   for line in mapping_dict:
     print 'Magic happening on line ' + str(counter) + " ", line, "..."
+
     if line['format'] == 'csv':
       download_file(line['url'], 'tmp_file')
       os.system('./tarql-1.1/bin/tarql --delimiter=comma ' + line['mapping'] + ' tmp_file >> target/' + transform_mapping_name(line['mapping']))
       os.remove('tmp_file')
+
     elif line['format'] == 'dsv':
       download_file(line['url'], 'tmp_file')
       os.system('./tarql-1.1/bin/tarql --delimiter=dash ' + line['mapping'] + ' tmp_file >> target/' + transform_mapping_name(['mapping']))
       os.remove('tmp_file')
+
     elif line['format'] == 'scsv':
       download_file(line['url'], 'tmp_file')
       os.system('./tarql-1.1/bin/tarql --delimiter=semicolon ' + line['mapping'] + ' tmp_file >> ' + transform_mapping_name(line['mapping']))
       os.remove('tmp_file')
+
     elif line['format'] == 'json':
       download_file(line['url'], 'tmp_file')
       os.system('java -jar jarql.jar tmp_file ' + line['mapping'] + ' >> ' + transform_mapping_name(line['mapping']))
       os.remove('tmp_file')
+
     else :
       print 'File format not found!!'
       sys.exit(1)
+
     counter += 1
     print "Done"
+
 print "The End!"    
